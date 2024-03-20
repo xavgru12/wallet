0119c892827b0fdd981ee257a5829034a1509d18
Xaver Gruber <xaver.max.gruber@gmail.com>
2024-03-20 19:02:21 +0100

    print keys and adress to file and generate qr for address

--- createWallet.py
+++ createWallet.py
@@ -3,6 +3,8 @@ import codecs
 import hashlib
 import base58
 from Crypto.Hash import RIPEMD160
+import qrcode
+import datetime
 
 def generate_secp256k1_keypair():
     private_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
@@ -17,10 +19,11 @@ def pubkey_to_address(pubkey_hex):
     pubkey_bytes = bytes.fromhex(pubkey_hex)
     sha256 = hashlib.sha256(pubkey_bytes).digest()
     ripemd160 = RIPEMD160.new(sha256).digest()
-    version = b'\x6f'  # Use '\x00' for mainnet addresses
+    version = b'\x00'  # Use '\x00' for mainnet addresses
     payload = version + ripemd160
     checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
     full_payload = payload + checksum
+
     address = base58.b58encode(full_payload).decode('utf-8')
     return address
 
@@ -31,3 +34,16 @@ print("Private Key (hex):", private_key_hex)
 print("Public Key (hex, compressed 33 bytes):", public_key_hex)
 address = pubkey_to_address(public_key_hex)
 print(f"address: {address}")
+
+img = qrcode.make(address)
+timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+file_name = f"address-{timestamp}"
+img.save(file_name)
+
+
+with open (f"secrets-{timestamp}.txt", "w+") as f:
+    f.write(f"private key: {private_key_hex}\n")
+
+    f.write(f"public_key_hex: {public_key_hex}\n")
+
+    f.write(f"address: {address}\n")
